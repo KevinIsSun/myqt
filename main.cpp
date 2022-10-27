2be8d9892ef54e746d64190daa9706de5702f59c
Kevin <kevin@naivesystems.com>
2022-10-27 18:24:28 +0800

    ptest 1

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    int i = 0;
 
     QTranslator translator;
     const QStringList uiLanguages = QLocale::system().uiLanguages();
